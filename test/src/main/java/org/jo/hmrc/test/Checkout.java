08eee7bccef9b08bd89b0c3c10e1a23d7dc95ce1
Jan Ostrowski <janostrowski@yahoo.co.uk>
2017-01-18 19:26:53 +0000

    updated Checkout class to support the offers

--- test/src/main/java/org/jo/hmrc/test/Checkout.java
+++ test/src/main/java/org/jo/hmrc/test/Checkout.java
@@ -5,8 +5,26 @@ import java.math.BigDecimal;
 public class Checkout {
 
     public BigDecimal calculateTotalCost(Basket basket) {
+        // would be more maintainable with Map<productId, State>, but with int[] is shorter
+        int[] offersState = { 0, 0 };
         return basket.getProducts().stream().
-                map(Product::getPrice).
+                map(product -> {
+                    switch (product.getId()) {
+                        case Shop.PRODUCT_ID_APPLE:
+                            if (++offersState[0] == 2) {
+                                offersState[0] = 0;
+                                return BigDecimal.ZERO;
+                            }
+                            break;
+                        case Shop.PRODUCT_ID_ORANGE:
+                            if (++offersState[1] == 3) {
+                                offersState[1] = 0;
+                                return BigDecimal.ZERO;
+                            }
+                    }
+                    System.out.println("adding" + product.getPrice());
+                    return product.getPrice();
+                }).
                 reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
