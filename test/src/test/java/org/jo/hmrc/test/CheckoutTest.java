d5bf8db8f516eb57e4e9ee0a1867c9a18d927f76
Jan Ostrowski <janostrowski@yahoo.co.uk>
2017-01-18 19:22:38 +0000

    updated CheckoutTest to reflect the offers

--- test/src/test/java/org/jo/hmrc/test/CheckoutTest.java
+++ test/src/test/java/org/jo/hmrc/test/CheckoutTest.java
@@ -13,10 +13,17 @@ public class CheckoutTest {
         Basket basket = new Basket();
         Checkout checkout = new Checkout();
         basket.add(shop.getProduct(Shop.PRODUCT_ID_APPLE));
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_ORANGE));
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_APPLE)); // free
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_ORANGE));
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_ORANGE)); // free
         basket.add(shop.getProduct(Shop.PRODUCT_ID_APPLE));
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_APPLE)); // free
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_ORANGE));
         basket.add(shop.getProduct(Shop.PRODUCT_ID_ORANGE));
         basket.add(shop.getProduct(Shop.PRODUCT_ID_APPLE));
+        basket.add(shop.getProduct(Shop.PRODUCT_ID_APPLE)); //free
         BigDecimal totalCost = checkout.calculateTotalCost(basket);
-        Assert.assertEquals(new BigDecimal("2.05"), totalCost);
+        Assert.assertEquals(new BigDecimal("2.80"), totalCost);
     }
 }
